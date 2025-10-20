7bead4ae031a676459acfbf77f8e6bc9eac7eaee
stellaras17 <steliosandronikou4@gmail.com>
2025-10-20 19:11:47 +0300

    api prod

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ export const MAX_URLS_PER_FILE = 7000;
 export const SITEMAP_DIR = '/sitemaps';
 export const SITE_URL = 'https://rsrvin.com';
 export const SITEMAP_URL = 'https://sitemap.rsrvin.com';
-export const API_URL = 'https://api-uat.rsrvin.com';
+export const API_URL = 'https://api.rsrvin.com';
 export const SITE_LANGS = ['en', 'el', 'ru']
 export const SITEMAP_SECRET = '8tcHH9QZzqcb/a5Jk05+tYsQ2626l2H1xto15JjEkzqI3MqotjX4tKWbh32MHHjg6S4H3of1xq4CLITqcEtaVA==';
